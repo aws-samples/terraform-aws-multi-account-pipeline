fd00d9d37e223e96fa3ed345266cd7d2068aca5e
jakebark <jakebark@amazon.co.uk>
2024-12-04 15:00:19 +0000

    var_explainer

--- example-code/locals.tf
+++ example-code/locals.tf
@@ -3,8 +3,12 @@
 
 # local.config can be used to reference specific values for each AWS account
 # eg local.config.cidr
-# you can use account_id or account_name in config.auto.tfvars
+# you can use account_id or account_name in config.auto.tfvars with the try function
 locals {
   config = var.config[try(var.account_id, var.account_name)]
 
+  # or just account_id or account_name
+
+  #config = var.config[var.account_id]
+  #config = var.config[var.account_name]
 }
