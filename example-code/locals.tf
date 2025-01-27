33aafeb785d9ca4dc524cc67a1102ab1de82c612
jakebark <jakebark@amazon.co.uk>
2025-01-27 06:32:20 +0000

    expanded example code

--- example-code/locals.tf
+++ example-code/locals.tf
@@ -1,6 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
+# default variable management
+
 # local.config can be used to reference specific values for each AWS account
 # eg local.config.cidr
 # you can use account_id or account_name in config.auto.tfvars with the try function
@@ -12,3 +14,5 @@ locals {
   #config = var.config[var.account_id]
   #config = var.config[var.account_name]
 }
+
+# remove the above if using workspaces directory
---
+++ example-code/workspaces/workload2.tfvars
@@ -0,0 +1,11 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+# alternative ariable management 
+# using workspace_directory input
+# workspace_directory = "workspaces"
+
+owner = "bob"
+cidr  = "10.21.192.0/24"
+
+# tfvars file can be account number or name
