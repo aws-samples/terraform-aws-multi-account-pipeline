84c3641c40a7d61dcbc1b81a91e9d4795d2d330e
jakebark <jakebark@amazon.com>
2025-04-16 07:29:30 +0100

    fixed checkov to 3.2.0

--- locals.tf
+++ locals.tf
@@ -16,6 +16,7 @@ locals {
     TFLINT_VERSION      = var.tflint_version
     SAST_REPORT_ARN     = aws_codebuild_report_group.sast.arn
     CHECKOV_SKIPS       = join(",", "${var.checkov_skip}")
+    CHECKOV_VERSION     = var.checkov_version
     WORKSPACE_DIRECTORY = var.workspace_directory
     TF_VERSION          = var.terraform_version
   }
