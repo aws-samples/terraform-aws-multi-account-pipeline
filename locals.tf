ba2b80a97e28a9df5f6b0bef52b9d8d9237284a8
jakebark <jakebark@amazon.com>
2025-04-16 08:03:41 +0100

    reorder env var

--- locals.tf
+++ locals.tf
@@ -13,12 +13,12 @@ locals {
   })
 
   env_var = {
-    TFLINT_VERSION      = var.tflint_version
-    SAST_REPORT_ARN     = aws_codebuild_report_group.sast.arn
     CHECKOV_SKIPS       = join(",", "${var.checkov_skip}")
     CHECKOV_VERSION     = var.checkov_version
-    WORKSPACE_DIRECTORY = var.workspace_directory
+    SAST_REPORT_ARN     = aws_codebuild_report_group.sast.arn
     TF_VERSION          = var.terraform_version
+    TFLINT_VERSION      = var.tflint_version
+    WORKSPACE_DIRECTORY = var.workspace_directory
   }
   conditional_env_var = merge(local.env_var, {
     TAGS           = var.tags
